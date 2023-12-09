f9e634f23cca97e8247f74be71198da98e1d7f04
Everett Griffiths <everett@fireproofsocks.com>
2023-12-09 08:19:29 -0500

    Day 8 test

---
+++ test/aoc/day8_test.exs
@@ -0,0 +1,10 @@
+defmodule Aoc.Day8Test do
+  use ExUnit.Case, async: false
+  alias Aoc.Day8
+
+  describe "solve_p12" do
+    test "correct" do
+      22411 = Day8.solve_pt1()
+    end
+  end
+end
