47774ffc521a2c63ed3462fde5af3dd061315caf
Everett Griffiths <everett@fireproofsocks.com>
2023-12-10 19:51:36 -0500

    Day 10 WIP ... broke

--- lib/aoc/day10.ex
+++ lib/aoc/day10.ex
@@ -6,99 +6,179 @@ defmodule Aoc.Day10 do
   https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
   """
 
-  @type coordinate :: {x :: non_neg_integer(), y :: non_neg_integer()}
-  @type grid :: %{required(coordinate) => Node.t()}
-
   defmodule Node do
+    alias Aoc.Day10.Grid
     @type t :: %__MODULE__{}
-    defstruct x: nil, y: nil, symbol: nil, visited?: false
+    @typedoc """
+    I'm abandoning the usual mathematical x,y stuff in favor of readability:
+    - `line` is the line number starting with 1
+    - `column` is the character offset within a given line, starting with 1
+    """
+    @type loc :: {line :: non_neg_integer(), column :: non_neg_integer()}
+    defstruct loc: nil, symbol: nil, visited?: false, distance: :infinity
+
+    @doc """
+    Get a MapSet (i.e. unordered set) of the nodes neighboring the given location
+    coordinate. Each point in the graph may have 2 to 4 neighbors. Corner points
+    have 2, edge points have 3, and all others have 4.
+    """
+    @spec neighbors(grid :: Grid.t(), loc :: loc()) :: MapSet.t()
+    def neighbors(grid, {line, col}) do
+      # north, south, east, west
+      [
+        Map.get(grid, {line - 1, col}),
+        Map.get(grid, {line + 1, col}),
+        Map.get(grid, {line, col + 1}),
+        Map.get(grid, {line, col - 1})
+      ]
+      |> Enum.reject(&is_nil/1)
+      |> MapSet.new()
+    end
+
+    def open_neighbors(grid, loc) do
+      this_node = Map.fetch!(grid, loc)
+      grid
+      |> neighbors(loc)
+      |> Enum.filter(fn next_node -> open?(this_node, next_node) end)
+      |> MapSet.new()
+    end
+
+    def unvisited_open_neighbors(grid, loc) do
+      grid
+      |> open_neighbors(loc)
+      |> Enum.reject(fn next_node -> next_node.visited? end)
+      |> MapSet.new()
+    end
+
+    # Connections -- is it possible to connect from node1 to node2?
+    # heading North, South, East, or West
+    def open?(%Node{loc: {l1, c1}, symbol: sym1}, %Node{loc: {l2, c2}, symbol: sym2})
+        when c1 == c2 and l1 - 1 == l2 and sym1 in ["S", "|", "J", "L"] and
+               sym2 in ["S", "|", "7", "F"],
+        do: true
+
+    def open?(%Node{loc: {l1, c1}, symbol: sym1}, %Node{loc: {l2, c2}, symbol: sym2})
+        when c1 == c2 and l1 + 1 == l2 and sym1 in ["S", "|", "F", "7"] and
+               sym2 in ["S", "|", "L", "J"],
+        do: true
+
+    def open?(%Node{loc: {l1, c1}, symbol: sym1}, %Node{loc: {l2, c2}, symbol: sym2})
+        when l1 == l2 and c1 + 1 == c2 and sym1 in ["S", "-", "L", "F"] and
+               sym2 in ["S", "-", "7", "J"],
+        do: true
+
+    def open?(%Node{loc: {l1, c1}, symbol: sym1}, %Node{loc: {l2, c2}, symbol: sym2})
+        when l1 == l2 and c1 - 1 == c2 and sym1 in ["S", "-", "J", "7"] and
+               sym2 in ["S", "-", "L", "F"],
+        do: true
+
+    def open?(_, _), do: false
+
+    def visit(grid, loc) do
+      this_node = Map.fetch!(grid, loc)
+      IO.inspect(this_node, label: "ARRIVING AT THIS NODE")
+      # Update this node as visited
+      grid = Grid.update!(grid, this_node, visited?: true)
+
+      open_neighbors = open_neighbors(grid, loc)
+      tentative_distance = this_node.distance + 1
+
+      IO.inspect(open_neighbors, label: "ALL OPEN NEIGHBORS (#{tentative_distance})")
+      # Update distances to all open neighbors, even ones we may have already visited!
+      # This is important so we can update them with the shortest path
+      grid =
+        open_neighbors
+        |> Enum.reduce(grid, fn next_node, grid ->
+          # Compare the newly calculated tentative distance to the one currently assigned
+          # to the neighbor and assign it the smaller one.
+          # tentative_distance = this_node.distance + 1
+
+          new_distance =
+            cond do
+              next_node.distance == :infinity -> tentative_distance
+              next_node.distance < tentative_distance -> next_node.distance
+              true -> tentative_distance
+            end
+
+          Grid.update!(grid, next_node, distance: new_distance)
+        end)
+
+      # Requery the grid to ensure we have updated data
+      unvisited_neighbors = unvisited_open_neighbors(grid, loc)
+
+      IO.inspect(unvisited_neighbors, label: "Unvisited Open NEIGHBORS")
+
+      unvisited_neighbors
+      |> Enum.reduce(grid, fn next_node, grid ->
+        visit(grid, next_node.loc)
+      end)
+    end
   end
+
+  defmodule Grid do
+    alias Aoc.Day10.Node
+
+    @type t :: %{required(Node.loc()) => Node.t()}
+
+    @doc """
+    Parses the given file as a grid of characters using line_number, char_offset
+    coordinates
+    """
+    @spec from_file(file :: String.t()) :: t()
+    def from_file(file) do
+      file
+      |> File.stream!()
+      |> Enum.with_index(1)
+      |> Enum.reduce(%{}, fn {line, line_number}, acc ->
+        line
+        |> String.trim()
+        |> String.graphemes()
+        |> Enum.with_index(1)
+        |> Enum.reduce(acc, fn {char, column}, acc ->
+          Map.put(acc, {line_number, column}, %Node{
+            loc: {line_number, column},
+            symbol: char
+          })
+        end)
+      end)
+    end
+
+    @spec start_node(grid :: t()) :: Node.t()
+    def start_node(grid) do
+      grid
+      |> Enum.find(:not_found, fn {_coords, node} -> node.symbol == "S" end)
+      |> elem(1)
+    end
+
+    @spec update!(grid :: t(), node :: Node.t(), new_data :: keyword()) :: t()
+    def update!(grid, %Node{loc: loc}, new_data) do
+      node = Map.fetch!(grid, loc)
+      Map.put(grid, loc, Enum.reduce(new_data, node, fn {k, v}, acc -> Map.put(acc, k, v) end))
+    end
+  end
+
   @doc """
 
-  There could be 4 paths out of the start node. 2 of them could be dead-ends.
-  It's also possible that 2 loops are defined (but perhaps the puzzle input isn't
-  that sinister?).
+  There COULD be 4 paths out of the start node (2 of them could be dead-ends, or
+  there could 2 loops are defined), but the puzzle input isn't that sinister: only
+  one loop is defined, so the S node has only 2 paths.
 
   Find the tile (i.e node) in the loop that is farthest from the starting position.
   ## Examples
 
-      iex> Aoc.Day10.solve_pt1("priv/day9_example.txt")
-      iex> Aoc.Day10.solve_pt1("priv/day9.txt")
+      iex> Aoc.Day10.solve_pt1("priv/day10_example.txt")
+      iex> Aoc.Day10.solve_pt1("priv/day10_ex1.txt")
+      iex> Aoc.Day10.solve_pt1("priv/day10.txt")
   """
-  def solve_pt1(input) do
-
-    grid = parse_as_grid(input)
-    dbg start = find_start(grid)
+  def solve_pt1(file) do
+    grid = Grid.from_file(file)
+    start_node = Grid.start_node(grid)
 
     grid
-    |> neighbors({start.x, start.y})
-    |> Enum.filter(fn node -> open?(start, node) end)
-    |> Enum.reject(fn node -> node.visited? end)
-
-    # |> Enum.reduce_while(fn node, acc ->
-    #   if x < 5 do
-    #     {:cont, acc + x}
-    #   else
-    #     {:halt, acc}
-    #   end
-    # end)
+    |> Grid.update!(start_node, distance: 0)
+    |> Node.visit(start_node.loc)
+    |> Enum.reject(fn {_, %Node{distance: distance}} -> distance == :infinity end)
+    |> Enum.map(fn {_, %Node{distance: distance}} -> distance end)
+    |> Enum.max()
   end
-
-  def traverse_path(node, path_acc) do
-
-  end
-  # Grid uses x, y coordinates:
-  # y = the line number (starting w 0)
-  # x = the character offset (starting w 0)
-  @spec parse_as_grid(file :: String.t()) :: grid()
-  def parse_as_grid(file) do
-    file
-    |> File.stream!()
-    |> Enum.with_index()
-    |> Enum.reduce(%{}, fn {line, y}, acc ->
-      line
-      |> String.trim()
-      |> String.graphemes()
-      |> Enum.with_index()
-      |> Enum.reduce(acc, fn {char, x}, acc ->
-        Map.put(acc, {x, y}, %Node{
-          x: x,
-          y: y,
-          symbol: char
-        })
-      end)
-    end)
-  end
-
-  @spec find_start(grid :: grid()) :: Node.t()
-  def find_start(grid) do
-    grid
-    |> Enum.find(fn {_coords, node} -> node.symbol == "S" end)
-    |> elem(1)
-  end
-
-  @doc """
-  Each point in the graph may have 2 to 4 neighbors. Corner points have 2,
-  edge points have 3, and all others have 4.
-  """
-  @spec neighbors(grid :: grid(), coordinate :: coordinate()) :: [coordinate()]
-  def neighbors(grid, {x, y}) do
-    # north, south, east, west
-    [
-      Map.get(grid, {x, y - 1}),
-      Map.get(grid, {x, y + 1}),
-      Map.get(grid, {x - 1, y}),
-      Map.get(grid, {x + 1, y})
-    ]
-    |> Enum.reject(&is_nil/1)
-  end
-
-
-  # Connections -- is it possible to connect from node1 to node2?
-  # heading North, South, East, or West
-  def open?(%Node{x: x1, y: y1, symbol: sym1}, %Node{x: x2, y: y2, symbol: sym2}) when x1 == x2 and (y1 - 1) == y2 and sym1 in ["S", "|", "J", "L"] and sym2 in ["S", "|", "7, F"], do: true
-  def open?(%Node{x: x1, y: y1, symbol: sym1}, %Node{x: x2, y: y2, symbol: sym2}) when x1 == x2 and (y1 + 1) == y2 and sym1 in ["S", "|", "F", "7"] and sym2 in ["S", "|", "L", "J"], do: true
-  def open?(%Node{x: x1, y: y1, symbol: sym1}, %Node{x: x2, y: y2, symbol: sym2}) when y1 == y2 and (x1 + 1) == x2 and sym1 in ["S", "-", "L", "F"] and sym2 in ["-", "7", "J"], do: true
-  def open?(%Node{x: x1, y: y1, symbol: sym1}, %Node{x: x2, y: y2, symbol: sym2}) when y1 == y2 and (x1 - 1) == x2 and sym1 in ["S", "-", "J", "7"] and sym2 in ["-", "L", "F"], do: true
-  def open?(_, _), do: false
 end
--- test/aoc/day10_test.exs
+++ test/aoc/day10_test.exs
@@ -1,21 +1,111 @@
 defmodule Aoc.Day10Test do
   use ExUnit.Case, async: false
   alias Aoc.Day10
+  alias Aoc.Day10.Grid
   alias Aoc.Day10.Node
 
-  describe "solve_p1/1" do
-    test "example" do
-      dbg Day10.solve_pt1("priv/day10_example.txt")
-      # assert map_size(grid) == 25
+  describe "solve_pt1/1" do
+    test "example 1 (simple)" do
+      assert 4 = Day10.solve_pt1("priv/day10_ex1.txt")
+    end
+
+    test "example 2 (a little harder)" do
+      assert 8 = Day10.solve_pt1("priv/day10_ex2.txt")
     end
   end
 
-  # This is some of the most important fundamental logic to the whole thing...
-  describe "open/2" do
-    test "foo" do
-      start = %Node{x: 1, y: 1, symbol: "S"}
-      north = %Node{x: 1, y: 0, symbol: "L"}
-      refute Day10.open?(start, north)
+  describe "Node.neighbors/2" do
+    test "ok" do
+      grid = Grid.from_file("priv/day10_ex2.txt")
+
+      node = %Node{
+        loc: {4, 2},
+        symbol: "F",
+        visited?: false,
+        distance: :infinity
+      }
+
+      expected =
+        MapSet.new([
+          %Node{loc: {3, 2}, symbol: "J"},
+          %Node{loc: {5, 2}, symbol: "J"},
+          %Node{loc: {4, 3}, symbol: "-"},
+          %Node{loc: {4, 1}, symbol: "|"}
+        ])
+
+      actual = Node.neighbors(grid, node.loc)
+      assert MapSet.equal?(actual, expected)
+    end
+  end
+
+  describe "Node.open_neighbors/2" do
+    test "ok 1" do
+      grid = Grid.from_file("priv/day10_ex2.txt")
+
+      node = %Node{
+        loc: {4, 2},
+        symbol: "F",
+        visited?: false,
+        distance: :infinity
+      }
+
+      expected =
+        MapSet.new([
+          %Node{loc: {5, 2}, symbol: "J"},
+          %Node{loc: {4, 3}, symbol: "-"}
+        ])
+
+      actual = Node.open_neighbors(grid, node.loc)
+      assert MapSet.equal?(actual, expected)
+    end
+
+    test "ok 2" do
+      grid = Grid.from_file("priv/day10_ex2.txt")
+
+      node = %Node{
+        loc: {3, 2},
+        symbol: "J"
+      }
+
+      expected =
+        MapSet.new([
+          %Node{loc: {3, 1}, symbol: "S"},
+          %Node{loc: {2, 2}, symbol: "F"}
+        ])
+
+      actual = Node.open_neighbors(grid, node.loc)
+      assert MapSet.equal?(actual, expected)
+    end
+  end
+
+  describe "Node.open?/2" do
+    test "J is open west to S" do
+      start = %Node{loc: {3, 2}, symbol: "J"}
+      west = %Node{loc: {3, 1}, symbol: "S"}
+      assert Node.open?(start, west)
+    end
+
+    test "J is open north to F" do
+      start = %Node{loc: {3, 2}, symbol: "J"}
+      north = %Node{loc: {2, 2}, symbol: "F"}
+      assert Node.open?(start, north)
+    end
+
+    test "S not open south to L" do
+      start = %Node{loc: {1, 1}, symbol: "S"}
+      north = %Node{loc: {1, 2}, symbol: "L"}
+      refute Node.open?(start, north)
+    end
+  end
+
+  describe "Grid.from_file/1" do
+    test "ok" do
+      assert %{
+               {1, 1} => %Node{loc: {1, 1}, symbol: "|"},
+               {1, 2} => %Node{loc: {1, 2}, symbol: "F"},
+               {2, 1} => %Node{loc: {2, 1}, symbol: "L"},
+               {2, 2} => %Node{loc: {2, 2}, symbol: "J"}
+             } = Grid.from_file("priv/day10_ex0.txt")
     end
   end
 end
